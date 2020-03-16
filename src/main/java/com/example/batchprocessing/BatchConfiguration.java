35d35ce753ab7eb5825e3d66078a6f058301bb87
rthompson <robb.thompson@osi.ca.gov>
2020-03-16 10:55:56 -0700

    more cleanup, fixing git version issues

--- src/main/java/com/example/batchprocessing/BatchConfiguration.java
+++ src/main/java/com/example/batchprocessing/BatchConfiguration.java
@@ -9,14 +9,6 @@ import java.util.Map;
 import java.util.Properties;
 import javax.sql.DataSource;
 
-import org.apache.http.NameValuePair;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.message.BasicNameValuePair;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
